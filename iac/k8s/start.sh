4503d5f078b57e8780a60c96707ba6a05cb9f0df
Glunde <-@vector.com>
2020-09-23 14:37:53 +0200

    config k8s manifests

--- iac/k8s/start.sh
+++
@@ -1,9 +0,0 @@
-kubectl apply -f secrets.yaml
-kubectl apply -f core-deploy.yaml
-sleep 5
-kubectl apply -f connection_pool-deploy.yaml
-sleep 10
-kubectl apply -f api-deploy.yaml
-kubectl apply -f scheduler-deploy.yaml
-sleep 20
-kubectl apply -f webgui-deploy.yaml
--- iac/k8s/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-kubectl delete deployment webgui --grace-period=0 
-kubectl delete deployment api --grace-period=0 
-kubectl delete deployment scheduler --grace-period=0 
-kubectl delete deployment core --grace-period=0 
-kubectl delete deployment connectionpool --grace-period=0 
-kubectl delete -f secrets.yaml --grace-period=0 
-kubectl delete -f core-deploy.yaml
