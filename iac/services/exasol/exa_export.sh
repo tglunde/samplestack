39fba41f030b769caf3d2680d80d505c006950b3
tglunde <torsten@glunde.de>
2020-10-01 06:55:34 +0200

    dvb exasol bootstrap k8s

---
+++ iac/services/exasol/exa_export.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DSN=localhost:8888
+USER=sys
+PWD=exasol
+EXAPLUS=exaplus
+
+$EXAPLUS -c $DSN -u $USER -p $PWD -sql 'drop table if exists db_history.database_ddl;' 
+$EXAPLUS -c $DSN -u $USER -p $PWD -sql 'execute script exa_toolbox.create_ddl(true,true,true);'
+$EXAPLUS -c $DSN -u $USER -p $PWD -sql "export (select ddl from db_history.database_ddl) into local csv file '$1' DELIMIT=NEVER;"
